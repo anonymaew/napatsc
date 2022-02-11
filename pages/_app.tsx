97023f3e97b6facc9c6e5816c8d149d79d6644d4
Napat Srichan <napatsrichan2001@gmail.com>
2022-02-11 00:51:54 -0800

    feat: :sparkles: add favicon
    add ns logo as favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          id="icon"
+          href="https://raw.githubusercontent.com/anonymaew/ns-logo/master/ns-logo-32-w.svg"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
